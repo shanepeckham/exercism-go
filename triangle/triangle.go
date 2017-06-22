6917a929602c26822c1dd4a1621265b2affabcb3
shanepeckham <shanepeckham@live.com>
2017-06-22 10:17:08 +0100

    Triangle

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,16 +1,66 @@
 package triangle
 
+import (
+	"math"
+)
+
 const testVersion = 3
 
-func KindFromSides(a, b, c float64) Kind
+type Kind string
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+const (
+	NaT Kind = "NaT"
+	Equ Kind = "Equ"
+	Iso Kind = "Iso"
+	Sca Kind = "Sca"
+)
 
-// Pick values for the following identifiers used by the test program.
-NaT // not a triangle
-Equ // equilateral
-Iso // isosceles
-Sca // scalene
+func CheckIsTriangle(a, b, c float64) bool {
 
-// Organize your code for readability.
+	var result = true
+
+	if math.Mod(a, a) != 0 || math.Mod(b, b) != 0 || math.Mod(c, c) != 0 {
+		result = false
+	}
+
+	if a+b+c <= 0 {
+		result = false
+	}
+
+	if a+b < c {
+		result = false
+	}
+
+	if a+c < b {
+		result = false
+	}
+
+	if b+c < a {
+		result = false
+	}
+
+	return result
+}
+
+func KindFromSides(a, b, c float64) Kind {
+
+	var result Kind
+
+	if a != b && b != c {
+		result = Sca
+	}
+
+	if (a == b || b == c) || (a == b || a == c) {
+		result = Iso
+	}
+
+	if a == b && a == c {
+		result = Equ
+	}
+
+	if !CheckIsTriangle(a, b, c) {
+		result = NaT
+	}
+
+	return result
+}
